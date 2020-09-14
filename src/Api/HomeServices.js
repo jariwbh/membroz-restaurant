dbc2e9c6e4f5782f253187eec5bad47659815b12
jariwbh <jariwbh@krtya.com>
2020-09-14 12:00:11 +0530

    HomeService-Filter

--- src/Api/HomeServices.js
+++ src/Api/HomeServices.js
@@ -2,12 +2,15 @@ import axios from '../Helpers/axiosInst'
 
 function getCategory(id) {
     const body = {
-        "search": [{
+        "search": [
+            {
             "searchfield": "formid",
             "searchvalue": "5e058897b0c5fb2b6c15cc69",
             "criteria": "eq",
             "datatype": "ObjectId"
-        }]
+        },
+        { "searchfield": "status", "searchvalue": "active", "criteria": "eq" }
+    ]
     }
 
     return axios.post('formdatas/filter', body);
