1a2609b0942261a3e1998313fb2255c23ea2a05e
sandipkrtya <sandip.asodariya@krtya.com>
2020-10-22 19:31:57 +0530

    Login className

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -102,7 +102,7 @@ class Login extends Component {
                             <button className="navbar-toggler navbar-toggler-login" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 {/* <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item"> <a class="nav-link white-link" href="#">Admin Login</a> </li>
+                                    <li className="nav-item"> <a className="nav-link white-link" href="#">Admin Login</a> </li>
                                 </ul> */}
                             </div>
                         </div>
@@ -149,16 +149,16 @@ class Login extends Component {
                                             <span className="help-block">{validation.password.message}</span>
                                         </div>
                                         <div className="form-group">
-                                            <div class="custom-control custom-checkbox">
+                                            <div className="custom-control custom-checkbox">
                                                 <input
-                                                    class="custom-control-input"
+                                                    className="custom-control-input"
                                                     type="checkbox"
                                                     id="rememberMe"
                                                     name="rememberme"
                                                     checked={rememberme}
                                                     onChange={this.onChangeValue}
                                                 />
-                                                <label class="custom-control-label" htmlFor="rememberMe">Remember me</label>
+                                                <label className="custom-control-label" htmlFor="rememberMe">Remember me</label>
                                                 {/* <Link className="float-right" to="/ForgetPassword">Forgot Password?</Link> */}
                                             </div>
                                         </div>
