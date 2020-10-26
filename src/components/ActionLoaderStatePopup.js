beeaf5e6c9b37ec0cda55ba99cf336736eff461f
sandipkrtya <sandip.asodariya@krtya.com>
2020-10-26 18:47:04 +0530

    ActionLoaderStatePopup

--- src/components/ActionLoaderStatePopup.js
+++ src/components/ActionLoaderStatePopup.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Modal } from "react-bootstrap";
 import '../Assets/css/ActionLoaderStatePopup.scss';
 
 export const LOADERSTATES = {
